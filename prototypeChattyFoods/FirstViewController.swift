2f9a6fd0e7b05931ed043b34810f8df02efa3250
kate <ekaterina.roberts@gmail.com>
2020-04-05 18:21:29 +0100

    Initial Commit

---
+++ prototypeChattyFoods/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  prototypeChattyFoods
+//
+//  Created by Kate Roberts on 05/04/2020.
+//  Copyright © 2020 Kate Roberts. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ prototypeChattyFoods/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  prototypeChattyFoods
+//
+//  Created by Kate Roberts on 05/04/2020.
+//  Copyright © 2020 Kate Roberts. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
